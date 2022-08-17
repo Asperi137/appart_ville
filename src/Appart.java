42d1972e94d47bbb8d9a5562712f8877fe324d9d
Jerome CHAPUT <106680913+Asperi137@users.noreply.github.com>
2022-08-17 13:57:59 +0200

    Initial commit

---
+++ src/Appart.java
@@ -0,0 +1,56 @@
+public class Appart {
+
+    private String noAppart;
+    private String noImmeuble;
+    private int nbPieces;
+    private float prix;
+    private float surface;
+
+    public Appart(String noAppart, String noImmeuble, int nbPieces, float prix, float surface) {
+        this.noAppart = noAppart;
+        this.noImmeuble = noImmeuble;
+        this.nbPieces = nbPieces;
+        this.prix = prix;
+        this.surface = surface;
+    }
+
+    public String getNoAppart() {
+        return noAppart;
+    }
+
+    public void setNoAppart(String noAppart) {
+        this.noAppart = noAppart;
+    }
+
+    public String getNoImmeuble() {
+        return noImmeuble;
+    }
+
+    public void setNoImmeuble(String noImmeuble) {
+        this.noImmeuble = noImmeuble;
+    }
+
+    public int getNbPieces() {
+        return nbPieces;
+    }
+
+    public void setNbPieces(int nbPieces) {
+        this.nbPieces = nbPieces;
+    }
+
+    public float getPrix() {
+        return prix;
+    }
+
+    public void setPrix(float prix) {
+        this.prix = prix;
+    }
+
+    public float getSurface() {
+        return surface;
+    }
+
+    public void setSurface(float surface) {
+        this.surface = surface;
+    }
+}
